0eeb64fc324b52cdbd37f0733ed738369f46c04c
Hannes Kirsman <hannes.kirsman@wunder.io>
2023-01-29 12:40:16 +0200

    #2 Show php errors in UI.

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -31,3 +31,11 @@ $settings['skip_permissions_hardening'] = TRUE;
 $settings['trusted_host_patterns'] = [
   'd8-high-contrast.lndo.site',
 ];
+
+
+// Show all errors.
+ini_set('error_reporting', E_ALL);
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+$config['system.logging']['error_level'] = 'verbose';
