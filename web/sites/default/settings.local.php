5cd792b2a51e45c101e06d469321b927326bb934
Hannes Kirsman <hannes.kirsman@wunder.io>
2023-02-18 20:50:18 +0200

    Show all errors in UI.

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -26,3 +26,11 @@ ini_set('memory_limit', '2G');
 
 // Don't harden the files folder locally.
 $settings['skip_permissions_hardening'] = TRUE;
+
+// Debug.
+// Show all errors.
+ini_set('error_reporting', E_ALL);
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+$config['system.logging']['error_level'] = 'verbose';
